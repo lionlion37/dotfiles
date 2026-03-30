d08968ca149e419cb66d5e5cd52814cfd56736b1
Lion Dungl <lidungl@archimedia.at>
2026-03-30 16:48:04 +0200

    Updated sway and waybar

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -17,6 +17,16 @@ map('v', 'k', 'j', opts)
 map('v', 'l', 'k', opts)
 map('v', 'ö', 'l', opts)
 
+-- Also apply to buffer / window switching
+map("n", "<C-j>", "<C-w>h", opts) -- window left
+map("n", "<C-k>", "<C-w>j", opts) -- window down
+map("n", "<C-l>", "<C-w>k", opts) -- window up
+map("n", "<C-ö>", "<C-w>l", opts) -- window right
+
+-- Buffer switching (previously H / L)
+map("n", "J", "<cmd>bprevious<cr>", opts)  -- was H (prev buffer)
+map("n", "Ö", "<cmd>bnext<cr>", opts)      -- was L (next buffer)
+
 -- Free up h, since this isn't used now anymore
 map('n', 'h', '<Nop>', opts)
 map('v', 'h', '<Nop>', opts)
